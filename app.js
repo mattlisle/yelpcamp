21565563827a06cc5bc8650d85e7e65ad8c6a575
Matt Lisle <matthew.j.lisle@gmail.com>
2020-08-01 15:13:02 -0700

    feat: basic campgrounds styling

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const campgrounds = [
+  { name: 'camp1', image: 'https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { name: 'camp2', image: 'https://images.pexels.com/photos/753603/pexels-photo-753603.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { name: 'camp3', image: 'https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { name: 'camp1', image: 'https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { name: 'camp2', image: 'https://images.pexels.com/photos/753603/pexels-photo-753603.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { name: 'camp3', image: 'https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&h=350' },
+];
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -14,12 +25,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    { name: 'camp1', image: 'https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&h=350' },
-    { name: 'camp2', image: 'https://images.pexels.com/photos/753603/pexels-photo-753603.jpeg?auto=compress&cs=tinysrgb&h=350' },
-    { name: 'camp3', image: 'https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&h=350' },
-  ];
   res.render('campgrounds', { campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => {
+  console.log('Received post request');
+  campgrounds.push({
+    name: req.body.name,
+    image: req.body.image,
+  });
+  res.redirect('campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
 });
 
 app.listen(PORT, HOST);
